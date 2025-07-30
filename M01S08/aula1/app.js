c09cfd70af75add196bc734558178a35a46718e4
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-07-29 21:14:03 -0300

    adiciona export de objeto

--- M01S08/aula1/app.js
+++ M01S08/aula1/app.js
@@ -1,5 +1,11 @@
-import { mensagem, mostraMensagem } from "./extra.js";
+import { mensagem, mostraMensagem, mostraArrow, mensagemObj } from "./extra.js";
 
 console.log("RODANDO...");
+
 console.log(mensagem);
+
 mostraMensagem();
+
+mostraArrow();
+
+console.log(mensagemObj);
--- M01S08/aula1/extra.js
+++ M01S08/aula1/extra.js
@@ -1,5 +1,13 @@
 export const mensagem = "Sou uma mensagem de outro arquivo.";
 
+export const mensagemObj = {
+  msg: "Sou uma mensagem em um objeto."
+};
+
 export function mostraMensagem() {
   console.log("Outra mensagem do arquivo extra.");
 }
+
+export const mostraArrow = () => {
+  console.log("Sou uma arrow function");
+};
