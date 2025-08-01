2ce6bc7c68b17dab4d3b037f945072aaf5aeb948
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-07-31 21:15:21 -0300

    adiciona exemplo de useState

--- M01S08/aula2react/src/pages/HomePage.jsx
+++ M01S08/aula2react/src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
+import { use, useState } from "react";
 import Header from "../components/Header";
 
 function HomePage() {
+  const [batataFrita, setBatataFrita] = useState(0);
+
+  function aumentaValor() {
+    setBatataFrita(batataFrita + 1);
+  }
+
   return (
-    <div>
-      {/* exemplo de comentário dentro do JSX */}
+    <div style={{ textAlign: "center" }}>
       <Header />
       <p>Página Inicial</p>
+      <button onClick={aumentaValor}>{batataFrita}</button>
     </div>
   );
 }
