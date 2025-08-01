78d7f5b996a40ddaf346bf97d9747c1e7fb469ed
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-07-31 21:33:39 -0300

    adiciona exemplo ciclo de vida

--- M01S08/aula2react/src/App.jsx
+++ M01S08/aula2react/src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import HomePage from "./pages/HomePage";
 
 function App() {
-  return <HomePage />;
+  const [page, setPage] = useState("home");
+
+  if (page === "home") {
+    return (
+      <div>
+        <HomePage />
+        <button onClick={() => setPage(null)}>Fecha</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button>Fecha</button>
+    </div>
+  );
 }
 
 export default App;
--- M01S08/aula2react/src/pages/HomePage.jsx
+++ M01S08/aula2react/src/pages/HomePage.jsx
@@ -1,12 +1,30 @@
-import { use, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 
 function HomePage() {
+  // este valor persiste entre os ciclos de vida
   const [batataFrita, setBatataFrita] = useState(0);
 
   function aumentaValor() {
+    // esta função altera o valor do state
     setBatataFrita(batataFrita + 1);
   }
+
+  useEffect(() => {
+    // rodar código na montagem do componente
+    console.log("MONTA_COMPONENTE");
+
+    return () => {
+      // rodar código na desmontagem do componente
+      console.log("DESMONTA_COMPONENTE");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("BATATA_FRITA_MUDOU");
+  }, [batataFrita]);
+
+  console.count("CICLO_DE_VIDA");
 
   return (
     <div style={{ textAlign: "center" }}>
