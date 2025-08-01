625d50c9f35de3c5ed6d3f1a6beaf82eb0b41d0f
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-01 19:59:04 -0300

    adiciona codigo do exercicio 2

--- M01S08/exercicioRestaurante/src/components/PageSection/PageSection.jsx
+++ M01S08/exercicioRestaurante/src/components/PageSection/PageSection.jsx
@@ -3,7 +3,7 @@ import "./PageSection.css";
 function PageSection({ id, title, className, hideTitle, children }) {
   return (
     <section className={`PageSection ${className}`}>
-      <div>
+      <div className="container">
         <h2 id={id} className={hideTitle ? "hidden" : ""}>
           {title}
         </h2>
--- M01S08/exercicioRestaurante/src/pages/HomePage.jsx
+++ M01S08/exercicioRestaurante/src/pages/HomePage.jsx
@@ -1,31 +1,26 @@
 import PageSection from "../components/PageSection/PageSection";
+import reataurantPhoto from "../assets/fachada_restaurante_exemplo.jpeg";
+
+import "./HomePage.css";
+
+/*
+Objetivo: Criar um componente de apresentação com layout dividido em duas seções.
+Requisitos:
+- Lado esquerdo: Frase principal: "Servindo massas há mais de 70 anos"
+                 Subfrase: "Qualidade passada por gerações"
+- Lado direito: Imagem fictícia representando a fachada do restaurante.
+Dica: Use CSS Flexbox ou Grid para dividir a tela horizontalmente.
+*/
 
 function HomePage() {
   return (
     <PageSection id="home" title="Início" className="HomePage" hideTitle>
-      <div className="RichText">
-        <p>
-          <strong>Objetivo:</strong> Criar um componente de apresentação com
-          layout dividido em duas seções.
-        </p>
-        <p>Requisitos:</p>
-        <ul>
-          <li>
-            <strong>Lado esquerdo:</strong> Frase principal:{" "}
-            <em>"Servindo massas há mais de 70 anos"</em>
-          </li>
-          <li>
-            <strong>Subfrase: </strong>
-            <em>"Qualidade passada por gerações"</em>
-          </li>
-          <li>
-            <strong>Lado direito:</strong> Imagem fictícia representando a
-            fachada do restaurante.
-          </li>
-        </ul>
-        <p>
-          Dica: Use CSS Flexbox ou Grid para dividir a tela horizontalmente.
-        </p>
+      <div className="left">
+        <p>Servindo massas há mais de 70 anos</p>
+        <p>Qualidade passada por gerações</p>
+      </div>
+      <div className="right">
+        <img src={reataurantPhoto} alt="Foto do Restaurante" />
       </div>
     </PageSection>
   );
