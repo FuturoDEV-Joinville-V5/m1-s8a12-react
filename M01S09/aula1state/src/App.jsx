58b3d2726dda62930b76762aa8b9ea0fb09570d2
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-05 19:52:29 -0300

    atualiza exemplo contador

--- M01S09/aula1state/src/App.jsx
+++ M01S09/aula1state/src/App.jsx
@@ -3,16 +3,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [contador, setContador] = useState(3);
+  const [contador, setContador] = useState(0);
 
+  // somando em cima da variavel contador
   const somaUm = () => {
     setContador(contador + 1);
+  };
+
+  // somando em cima do valor do state que vem no parametro da função
+  const somaUmComFuncao = () => {
+    setContador((valorAtual) => valorAtual + 1);
   };
 
   return (
     <div>
       <p>Contagem total: {contador}</p>
-      <button onClick={somaUm}>+1</button>
+      <button onClick={somaUmComFuncao}>+1</button>
     </div>
   );
 }
