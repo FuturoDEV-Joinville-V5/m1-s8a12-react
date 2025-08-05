872c4094341cab76b2c999dd8075c06b8c34233b
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-05 19:38:54 -0300

    adiciona logica de contador

--- M01S09/aula1state/src/App.jsx
+++ M01S09/aula1state/src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div>oi</div>;
+  const [contador, setContador] = useState(3);
+
+  const somaUm = () => {
+    setContador(contador + 1);
+  };
+
+  return (
+    <div>
+      <p>Contagem total: {contador}</p>
+      <button onClick={somaUm}>+1</button>
+    </div>
+  );
 }
 
 export default App;
