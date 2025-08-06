eae8ce21a6f079cd6a33f40df3ec7c2384039d99
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-05 21:08:26 -0300

    adiciona funcionalidade de criar tarefas

--- M01S09/aula1todolist/src/App.jsx
+++ M01S09/aula1todolist/src/App.jsx
@@ -3,16 +3,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const listaTarefas = [
-    { id: 1, description: "item de tarefa 1" },
-    { id: 2, description: "item de tarefa 2" },
-    { id: 3, description: "item de tarefa 3" },
-  ];
+  const [entrada, setEntrada] = useState("");
+  const [tarefas, setTarefas] = useState([
+    { id: 1, description: "tarefa inicial exemplo" },
+  ]);
+
+  const atualizaEntrada = (event) => {
+    setEntrada(event.target.value);
+  };
+
+  const adicionaTarefa = () => {
+    const novaTarefa = {
+      id: Date.now(),
+      description: entrada,
+    };
+    const novoTarefas = [...tarefas, novaTarefa];
+    setTarefas(novoTarefas);
+    setEntrada("");
+  };
 
   return (
     <div>
+      <h1>todolist</h1>
+      <input type="text" value={entrada} onChange={atualizaEntrada} />
+      <button onClick={adicionaTarefa}>+</button>
       <ul>
-        {listaTarefas.map((tarefa) => (
+        {tarefas.map((tarefa) => (
           <li key={tarefa.id}>{tarefa.description}</li>
         ))}
       </ul>
