bcfe6c5c638b9f103b54d4297409f8ae8f37d4ac
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-07 19:42:51 -0300

    adiciona effect de execução unica

--- M01S09/aula2effect/src/App.jsx
+++ M01S09/aula2effect/src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <p>Vazio</p>;
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    console.count("EXECUTOU_O_EFFECT");
+  }, []);
+
+  console.count("RODOU_LIFE_CYCLE");
+
+  return (
+    <div className="App">
+      <p>{contador}</p>
+      <button onClick={() => setContador(contador + 1)}>+1</button>
+    </div>
+  );
 }
 
 export default App;
