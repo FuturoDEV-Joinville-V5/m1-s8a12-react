fcfeadf715a6f08ac9c7d7cbc4875693e982b439
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-08 19:27:15 -0300

    adiciona exemplo controle input

---
+++ M01S09/aula3inputcontrol/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <label htmlFor="nome">Nome:</label>
+      <input id="nome" type="text" />
+    </div>
+  );
+}
+
+export default App;
