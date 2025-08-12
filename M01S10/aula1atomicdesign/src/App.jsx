a8c0283f9ad6574954db9913a56883b2d26fc815
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-12 19:51:03 -0300

    adiciona atom typografy

--- M01S10/aula1atomicdesign/src/App.jsx
+++ M01S10/aula1atomicdesign/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <p>Atomic Design</p>
-      <Button variant="danger" size="sm" text="Nosso Botão Atom" />
+      <Button size="sm" text="Nosso Botão Atom" />
     </div>
   );
 }
---
+++ M01S10/aula1atomicdesign/src/components/atoms/Typografy.jsx
@@ -0,0 +1,26 @@
+export function Typografy({
+  variant = "body",
+  text,
+  className = "",
+  bold = false,
+}) {
+  const Tag = ["body", "caption"].includes(variant) ? "p" : variant;
+
+  const variantClasses = {
+    h1: "display-4",
+    h2: "display-5",
+    h3: "display-6",
+    body: "",
+    caption: "small text-muted",
+  };
+
+  return (
+    <Tag
+      className={`${variantClasses[variant]} ${
+        bold ? "fw-bold" : ""
+      } ${className}`}
+    >
+      {text}
+    </Tag>
+  );
+}
