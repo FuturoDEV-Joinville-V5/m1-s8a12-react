3558572b79c8e4cb531cecc0a3f534b017df88b7
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-12 19:48:14 -0300

    adiciona primeiro atom

---
+++ M01S10/aula1atomicdesign/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/atoms/Button";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Atomic Design</p>
+      <Button variant="danger" size="sm" text="Nosso Botão Atom" />
+    </div>
+  );
+}
+
+export default App;
---
+++ M01S10/aula1atomicdesign/src/components/atoms/Button.jsx
@@ -0,0 +1,29 @@
+export function Button({
+  text,
+  onClick = () => {},
+  variant = "primary",
+  size = "md",
+  className = "",
+}) {
+  const variantStyles = {
+    primary: { backgroundColor: "green" },
+    secondary: { backgroundColor: "orange" },
+    danger: { backgroundColor: "red" },
+  };
+
+  const sizeStyles = {
+    sm: { padding: "2px" },
+    md: { padding: "4px" },
+    lg: { padding: "8px" },
+  };
+
+  return (
+    <button
+      className={`btn ${className}`}
+      style={{ ...variantStyles[variant], ...sizeStyles[size] }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
