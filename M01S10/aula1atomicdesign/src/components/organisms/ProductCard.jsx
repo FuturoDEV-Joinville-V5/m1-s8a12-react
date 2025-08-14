efb4dbcd557132cb43b883ffec34590adceea723
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-14 19:19:45 -0300

    adiciona material icon ao exemplo da aula anterior

--- M01S10/aula1atomicdesign/src/components/organisms/ProductCard.jsx
+++ M01S10/aula1atomicdesign/src/components/organisms/ProductCard.jsx
@@ -1,19 +1,17 @@
+import StarIcon from "@mui/icons-material/Star";
 import Typografy from "../atoms/Typografy";
 import Button from "../atoms/Button";
 
 export default function ProductCard({ product = {}, onAddToCart = () => {} }) {
-  const renderStars = (rating) => {
-    let stars = "";
-    for (let i = 0; i < rating; i += 1) {
-      stars += "⭐";
-    }
-    return stars;
-  };
-
   return (
     <div className="ProductCard">
       <Typografy text={product.name} />
-      <div>{renderStars(product.rating)}</div>
+      <div style={{ display: "flex" }}>
+        <div style={{ color: "orange" }}>
+          <StarIcon />
+        </div>
+        {product.rating}
+      </div>
       <Button text="Adicinar ao carrinho" onClick={onAddToCart} />
     </div>
   );
