1460cecd7ecee0e8bad68dd10882cb5598538e60
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-19 19:49:50 -0300

    adiciona rotas no projeto

--- M01S11/aula1reactrouterdom/src/App.jsx
+++ M01S11/aula1reactrouterdom/src/App.jsx
@@ -1,14 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MoviesPage from "./pages/MoviesPage";
 import ContactPage from "./pages/ContactPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <MoviesPage />
-      <ContactPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ M01S11/aula1reactrouterdom/src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+  return (
+    <div className="NotFoundPage">
+      <p>404: Página não encontrada!</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
