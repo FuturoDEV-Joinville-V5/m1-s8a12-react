9d376ab13de6f1b747084b311b0d8d6357d813b6
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-22 20:08:55 -0300

    adiciona exemplo use ref

--- M01S11/aula3revisao/src/pages/InicioPagina.jsx
+++ M01S11/aula3revisao/src/pages/InicioPagina.jsx
@@ -1,8 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ConjuntoCampo from "../components/molecules/ConjuntoCampo";
 
 function InicioPage() {
+  // maneira de persistir valores e monitorar mudanças
   const [nome, setNome] = useState("");
+
+  // maneira de persistir valores sem monitorar mudanças
+  // acessa ou altera valores pelo .current
+  const batataRef = useRef("VALOR");
 
   useEffect(() => {
     console.log("MONTOU O COMPONENTE");
@@ -15,11 +20,11 @@ function InicioPage() {
     console.log("NOME MUDOU");
   }, [nome]);
 
-  console.log("RODOU LIFE CICLE");
+  console.log("RODOU LIFE CICLE", batataRef.current);
 
   return (
     <div className="InicioPage">
-      <h1>Revisao React</h1>
+      <h1 ref={batataRef}>Revisao React</h1>
       <ConjuntoCampo
         rotulo="Nome"
         id="nome"
