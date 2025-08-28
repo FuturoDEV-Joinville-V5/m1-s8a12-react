46cd073806c332766bbd1ca792a591f61e5bc888
romeuic <romeu.casarotto@edu.sc.senai.br>
2025-08-28 19:49:07 -0300

    adiciona navbar

--- M01S12/projetoexemplo/src/components/Navbar.jsx
+++ M01S12/projetoexemplo/src/components/Navbar.jsx
@@ -1,15 +1,32 @@
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import logoImg from "../assets/logo32.png";
+
 import styles from "./Navbar.module.css";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // código para fazer logout
+    navigate("/");
+  };
+
   return (
     <nav className={styles.Navbar}>
-      <div>
+      <Link to="/dashboard" className={styles.LogoBox}>
+        <img src={logoImg} alt="Imagem Logo" />
         <h1>Busca Oficinas</h1>
-      </div>
+      </Link>
       <ul>
-        <li>Dashboard</li>
-        <li>Oficinas</li>
-        <li>Sair</li>
+        <li>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </li>
+        <li>
+          <NavLink to="/workshops">Oficinas</NavLink>
+        </li>
+        <li>
+          <button onClick={handleLogout}>Sair</button>
+        </li>
       </ul>
     </nav>
   );
